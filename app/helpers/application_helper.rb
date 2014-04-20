bea3011b20a61f17dd0d185991e75c0c260d3212
stevenson.jean-pierre@sage.com <stevenson.jean-pierre@sage.com>
2014-04-20 00:51:15 -0400

    fixed some things

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def title(page_title)
+    content_for(:title) { page_title.to_s }
+  end
+
+  def yield_or_default(section, default = '')
+    content_for?(section) ? content_for(section) : default
+  end
+
+  def active_nav(actionName)
+    if params[:action] == actionName
+      'active'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
 
+  resources :users
+
+  root 'github#index'
+
+  #login logout authentication logic
+  match '/logout' => 'sessions#destroy', via: %i(get delete), as: :logout
+  match '/login' => 'sessions#new', via: %i(get), as: :login
+  match '/auth/:service/callback' => 'services#create', via: %i(get post)
+  match '/auth/failure' => 'services#failure', via: %i(get post)
+  resources :services, only: %i(index create destroy)
 
 
 
